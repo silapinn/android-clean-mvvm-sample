cf616f66b745375b8a01fb7fea05f47d0c2f3ead
Silapin Narthasilpa <silapin.narthasilpa@accenture.com>
2022-05-01 04:47:58 +0700

    feat: create common price change text view

---
+++ app/src/main/java/com/example/cryptocurrency/common/widget/PriceChangeTextView.kt
@@ -0,0 +1,41 @@
+package com.example.cryptocurrency.common.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import com.example.cryptocurrency.R
+
+class PriceChangeTextView(
+    context: Context,
+    attrs: AttributeSet
+) : AppCompatTextView(context, attrs) {
+
+    var change: Double = 0.0
+        set(value) {
+            field = value
+            updateComponents(value)
+        }
+
+    private fun updateComponents(change: Double) {
+        if (change >= 0) {
+            setTextColor(ContextCompat.getColor(context, R.color.green))
+            setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_arrow_up,
+                0,
+                0,
+                0
+            );
+        } else {
+            setTextColor(ContextCompat.getColor(context, R.color.red))
+            setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_arrow_down,
+                0,
+                0,
+                0
+            );
+
+        }
+        text = change.toString()
+    }
+}
